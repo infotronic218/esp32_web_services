7c136d5ecc44b9a6438e519fee789d6ffce59a0b
infotronic218 <infotronic218@gmail.com>
2023-09-30 23:54:45 +0200

    Project Initialization

--- main/main.c
+++ main/main.c
@@ -2,9 +2,32 @@
 #include<freertos/FreeRTOS.h>
 #include<freertos/FreeRTOSConfig.h>
 #include<freertos/task.h>
+#include"driver/gpio.h"
+
+#define LED 4 
+void led_task(void *params);
+
 
 
 void app_main(void)
 {
+    xTaskCreate(led_task, "LED Blinking", 1024, NULL, 5, NULL);
+
+}
+
+
+
+
+void led_task(void *params ){
+    // Pin configure 
+    gpio_set_direction(LED, GPIO_MODE_OUTPUT);
+    static uint8_t state = 0;
+
+    while(1)
+    {
+        vTaskDelay(1000);
+        gpio_set_level(LED, state);
+
+    }
 
 }
