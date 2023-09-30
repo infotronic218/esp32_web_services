535205370ef71cf79c023a6d45a7965e8f2437f9
infotronic218 <infotronic218@gmail.com>
2023-10-01 00:02:06 +0200

    Update the code

--- main/main.c
+++ main/main.c
@@ -4,7 +4,7 @@
 #include<freertos/task.h>
 #include"driver/gpio.h"
 
-#define LED 4 
+#define LED 2
 void led_task(void *params);
 
 
@@ -12,6 +12,10 @@ void led_task(void *params);
 void app_main(void)
 {
     xTaskCreate(led_task, "LED Blinking", 1024, NULL, 5, NULL);
+
+    while(1){
+        vTaskDelay(5000);
+    }
 
 }
 
@@ -25,8 +29,9 @@ void led_task(void *params ){
 
     while(1)
     {
-        vTaskDelay(1000);
+        vTaskDelay(500);
         gpio_set_level(LED, state);
+        state =! state ;
 
     }
 
