94caf15cdfc4f9c18b2ff3f2450dfa97ea3fc12c
infotronic218 <infotronic218@gmail.com>
2023-10-19 20:54:53 +0200

    Bootstrap menubar issue

--- web_pages/esp_vue/src/App.vue
+++ web_pages/esp_vue/src/App.vue
@@ -7,18 +7,18 @@ import { RouterLink, RouterView } from 'vue-router'
      <div class="row">
       <nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
         <a class="navbar-brand" href="/"> <strong>VUE ESP32</strong></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-bs-controls="navbarText" aria-bs-expanded="false" aria-bs-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
           <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
+            <li class="nav-item active m-1">
               <RouterLink  class="btn btn-primary mx-2" to="/">Home</RouterLink>
             </li>
-            <li class="nav-item">
+            <li class="nav-item m-1">
               <RouterLink class="btn btn-warning mx-2" to="/leds">LED</RouterLink>
             </li>
-            <li class="nav-item">
+            <li class="nav-item m-1">
               <RouterLink class="btn btn-secondary mx-2" :to="{name:'WiFi'}">WiFi</RouterLink>
             </li>
           </ul>
--- web_pages/esp_vue/src/main.js
+++ web_pages/esp_vue/src/main.js
@@ -1,6 +1,10 @@
 
+import  "@popperjs/core"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap'
+;
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
