6ab5b0677868de0d236e9bd24906d8bb69a6165d
infotronic218 <infotronic218@gmail.com>
2023-10-20 21:12:41 +0200

    Update the bootstrap integration

--- web_pages/esp_vue/src/views/LedView.vue
+++ web_pages/esp_vue/src/views/LedView.vue
@@ -1,71 +1,65 @@
 <template>
   <div class="container">
-    <h1 class="text-center text-uppercase">LED page</h1>
-    <div class="row">
-         <div class="col-sm-4 m-1">
-            <input v-model="number1" type="number"  class="form-control">
-         </div>
-         <div class="col-sm-4 m-1">
-            <input v-model="number2" type="number"  class="form-control">
-         </div>
-         <div class="col-sm-4">
-            <label>Result: {{ result }}</label>
-         </div>
-    </div>
-    <div class="row">
-        <button v-on:click="set_led_state(true)" class="col-sm-2 m-1 btn btn-success">ON</button>
-        <button v-on:click="set_led_state(false)"  class="col-sm-2 m-1 btn btn-danger">OFF</button>
-    </div>
+    <h1 class="text-center">LED control page</h1>
 
-    <div class="form-check form-switch">
-  <input class="form-check-input"   type="checkbox" v-model="led_state" role="switch" id="flexSwitchCheckDefault">
-  <label class="form-check-label" for="flexSwitchCheckDefault">LED State {{ led_state }}</label>
-
-</div>
+    <div class="row">
+      <div class="col-sm-4">
+        <button
+          v-on:click="set_led_state(true)"
+          class="col-sm-2 m-1 btn btn-success"
+        >
+          ON
+        </button>
+        <button
+          v-on:click="set_led_state(false)"
+          class="col-sm-2 m-1 btn btn-danger"
+        >
+          OFF
+        </button>
+      </div>
+      <div class="col-sm-2">
+        <div class="form-check form-switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            v-model="led_state"
+            role="switch"
+            id="flexSwitchCheckDefault"
+          />
+          <label class="form-check-label" for="flexSwitchCheckDefault"
+            >LED State {{ led_state }}</label
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script setup>
-import { onMounted, ref, watch } from 'vue'
-import axios from 'axios';
+import { onMounted, ref, watch } from "vue";
+import axios from "axios";
+const led_state = ref(false);
 
-const number1 = ref(0)
-const number2 = ref(0)
-const result = ref(0)
-const led_state = ref(false)
-watch(number1, async (newNumber, oldNumber) => {
-    result.value = newNumber + number2.value;
-})
+onMounted(() => {
+  axios.get("/api/led_command").then((response) => {
+    console.log("Getting the LED state from the server", response.data);
+    led_state.value = response.data.state;
+  });
+});
 
-watch(number2, async (newNumber, oldNumber) => {
-  result.value= newNumber + number1.value;
-  console.log(result.value)
-})
-
-
-onMounted(()=>{
-   axios.get("/api/led_command").then(response=>{
-    console.log('Getting the LED state from the server',response.data)
-    led_state.value = response.data.state ;
-   })
-})
-
-const set_led_state = async function(state){
-  console.log(state)
+const set_led_state = async function (state) {
+  console.log(state);
   let data = {
     name: "led1",
-    state:state
-  }
-  
+    state: state,
+  };
 
-  axios.post("/api/led_command", data).then((response)=>{
-    console.log(response.data)
-    led_state.value = response.data.state ;
-  })
-/*const response =  await fetch('/api/led_command');
+  axios.post("/api/led_command", data).then((response) => {
+    console.log(response.data);
+    led_state.value = response.data.state;
+  });
+  /*const response =  await fetch('/api/led_command');
   const data_jon  = await response.json();
   console.log(data_jon);
 */
-  
-}
+};
 </script>
-
